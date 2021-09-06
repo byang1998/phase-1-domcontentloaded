450ab01f9e523fff5f8ac9a925e04fb84a52215e
Ben Yang <benyang2020@gmail.com>
2021-09-06 18:51:59 -0400

    commit tree 88aa8631719b100b2ff57aa4d8d27a4fa3b5fbe6

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    newdom()
+} );
+
+function newdom () {
+    document.getElementById("text")
+    .innerHTML = 'This is really cool!'
+}
+
+
